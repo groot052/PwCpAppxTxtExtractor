5201dbfc813cb7168487b62d2403d0dde0376e00
pradeepkhadaw <72299349+pradeepkhadaw@users.noreply.github.com>
2025-05-04 08:49:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "23000618"
 api_hash = "da88b4f9c15b2860271e086444346a93"
 bot_token = "8174804084:AAEBOn2XlqGEsvXyvsFI35w6aKTZJaqYaKY"
-auth_users = [877609705,8105183992,1265309848]
+auth_users = [877609705]
